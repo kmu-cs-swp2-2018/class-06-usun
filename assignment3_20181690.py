e42130bf9fbca87388aea99f94d7d1f61d6825c4
yoosun813 <usun18813@kookmin.ac.kr>
2018-09-12 15:43:27 +0900

    assignment3 코드 수정

--- assignment3_20181690.py
+++ assignment3_20181690.py
@@ -15,6 +15,8 @@ def readScoreDB():
 
     try:
         scdb =  pickle.load(fH)
+        for i in scdb:
+            i['Score'] = int(i['Score'])  # inc실행 시 기존에 있던 이름에도 점수를 추가할 수 있도록 int로 읽어옴
     except:
         print("Empty DB: ", dbfilename)
     else:
@@ -38,22 +40,30 @@ def doScoreDB(scdb):
 
 # add
         if parse[0] == 'add':
-            if len(parse) == 4:
-                try:
-                    record = {'Name': parse[1], 'Age': int(parse[2]), 'Score': int(parse[3])}
-                    # 공백 기준 (2번째 단어 = Name value), (3번째 = Age value), (4번째 = Score value)로 dic record에 저장
-                    scdb += [record]
-                except:  # 에러처리 : Age, Score의 value가 정수가 아닐 때
-                    print("나이(정수 값), 성적(정수 값)을 정확히 입력해주세요.")
-            else:      # 에러처리 : value에 입력되지 않은 값이 있을 때
-                print ("이름, 나이, 성적을 정확히 입력해주세요.")
+            try:
+                record = {'Name': parse[1], 'Age': int(parse[2]), 'Score': int(parse[3])}
+                # 공백 기준 (2번째 단어 = Name value), (3번째 = Age value), (4번째 = Score value)로 dic record에 저장
+                scdb += [record]
+            except ValueError:  # 에러처리 : Age, Score의 value가 정수가 아닐 때
+                print ("나이, 성적을 정수로 입력해주세요.")
+            except IndexError:  # 에러처리 : 입력되지 않은 값이 있을 때
+                print("이름, 나이, 성적을 정확히 입력해주세요.")
+
 
 # del 명령 수정
         elif parse[0] == 'del':
-            for i in scdb:      # 이중 for문으로 name 같은 모든 사람 레코드 제거
-                for p in scdb:
+            try:
+                for p in scdb[:]:     # name 같은 모든 사람 레코드 제거
                     if p['Name'] == parse[1]:
                         scdb.remove(p)
+                    else:     # 에러처리 : 잘못된 이름을 입력했을 때
+                        print('삭제할 이름을 정확히 입력해주세요.')            
+
+
+            except IndexError:   # 에러처리 : 이름을 입력하지 않았을 때
+                print('삭제할 이름을 입력해주세요.')
+
+
 
 # show
         elif parse[0] == 'show':
@@ -62,39 +72,42 @@ def doScoreDB(scdb):
 
 # find 명령 추가
         elif parse[0] == 'find':
-            if len(parse) == 2:    # 입력값이 find, name 2개 일 경우
+            try :
                 for p in scdb:
                     if p['Name'] == parse[1]:
                         for attr in p:
-                            p[attr] = str(p[attr])     # int + str 하면 에러가 나므로 Age, Score를 str로 변환
-                            print(attr + "=" + p[attr], end=' ')
+                            print(attr + "={0}".format(p[attr]), end=' ')
                         print()
-                    elif p['Name'] != str(parse[1]):     # 에러처리 : 이름을 숫자로 입력했을 때
-                        print('이름을 정확히 입력해주세요.')
-                        break
-            else:
-                print('이름을 정확히 입력해주세요.')
+                    else:     # 에러처리 : 잘못된 이름을 입력했을 때
+                        print('검색할 이름을 정확히 입력해주세요.')
+                    break
+            except IndexError:    # 에러처리 : 이름을 입력하지 않았을 때
+                print('이름을 입력해주세요.')
+
 
 # inc 명령 추가
         elif parse[0] == 'inc':
-            if len(parse) == 3:     # 입력값이 inc, Name, amount 일 경우
+            try:
                 amount = int(parse[2])
-                for i in scdb:
-                    i['Age'] = int(i['Age'])
-                    i['Score'] = int(i['Score'])
                 for p in scdb:
                     if p['Name'] == parse[1]:
                         p['Score'] += amount
-                        for attr in p:
-                            p[attr] = str(p[attr])
-                            print(attr + "=" + p[attr], end=' ')
+                        for attr in sorted(p) :
+                            print(attr + "={}".format(p[attr]), end=' ')
                         print()
-            else:
+                    else:     # 에러처리 : 잘못된 이름을 입력했을 때
+                        print('점수를 추가할 이름을 정확히 입력해주세요.')
+                    break
+            except ValueError:     # 에러처리 : 추가할 점수가 정수가 아닐 때
                 print('점수를 정확히 입력해주세요.')
+            except IndexError:  # 에러처리 : 입력되지 않은 값이 있을 때
+                print('이름,점수를 정확히 입력해주세요.')
+
 
 # quit
         elif parse[0] == 'quit':
             break
+
 
         else:
             print("Invalid command: " + parse[0])
@@ -103,8 +116,7 @@ def doScoreDB(scdb):
 def showScoreDB(scdb, keyname):
     for p in sorted(scdb, key=lambda person: person[keyname]):
         for attr in sorted(p):
-            p[attr] = str(p[attr])     # int + string이 안되므로 바꿔줌
-            print(attr + "=" + p[attr], end=' ')
+            print(attr + "={0}".format(p[attr]), end=' ')
         print()
 
 
