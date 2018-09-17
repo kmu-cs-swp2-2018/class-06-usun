d0526b752806abb1d47661f4856e5f51ba299d74
yoosun813 <usun18813@kookmin.ac.kr>
2018-09-17 21:39:49 +0900

    assn2 이진탐색 변수 이름 변경, assn2-2 피보나치 파일 추가

---
+++ assignment2-2_20181690.py
@@ -0,0 +1,43 @@
+#20181690 정유선
+
+import time
+import random
+
+
+#재귀적 구현
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+
+#반복적 구현
+def iterfibo(n):
+    num0 = 0
+    num1 = 1
+    num2 = 1
+
+    if n == 0 :
+        return 0
+    elif n == 1 :
+        return 1
+    else:
+        for i in range(2, n+1):
+            num2 = num1
+            num1 = num0 + num1
+            num0 = num2
+    return num1
+
+
+while True:
+    n = int(input("Enter a number: "))
+    if n == -1:
+        break
+    ts = time.time()
+    fibonumber = iterfibo(n)
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" %(n, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(n)
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" %(n, fibonumber, ts))
--- assignment2_20181690.py
+++ assignment2_20181690.py
@@ -8,20 +8,17 @@ def seqsearch(nbrs, target):
             return i
     return -1
 
-def recbinsearch(L, l, u, target):
-    # L: 탐색 대상이 되는 리스트
-    # l: 탐색 범위의 왼쪽 (아래쪽) 끝 (lower)
-    # u: 탐색 범위의 오른쪽 (위쪽) 끝 (upper)
-    # target: 탐색하고자 하는 값 (key)
+def recbinsearch(searchList, lower, upper, target):
+
     return -1
 
-    middlenum = (l+u)//2
-    if target == L[middlenum]:
-        return L(middlenum)
-    elif target < L[middlenum]:
-        return recbinsearch(L, l, middlenum-1, target)
+    middlenum = (lower+upper)//2
+    if target == searchList[middlenum]:
+        return (middlenum)
+    elif target < searchList[middlenum]:
+        return recbinsearch(searchList, lower, middlenum-1, target)
     else :
-        return recbinsearch(L, middlenum + 1, l, target)
+        return recbinsearch(searchList, middlenum + 1, lower, target)
 
 
 
