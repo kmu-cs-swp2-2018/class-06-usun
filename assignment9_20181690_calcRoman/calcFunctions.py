7cd770363bf8f6ba9775a71164fde0def35bbafb
yoosun813 <usun18813@kookmin.ac.kr>
2018-10-25 23:51:42 +0900

    romanToDec 우선 if문으로 구현, 9로 끝나는 숫자들 잘 못 변환된다..ㅠㅜ

--- assignment9_20181690_calcRoman/calcFunctions.py
+++ assignment9_20181690_calcRoman/calcFunctions.py
@@ -47,10 +47,38 @@ def decToRoman(numStr):
 
 def romanToDec(numStr):
 
-        n = int(numStr)
-        result = ""
+        s = str(numStr)
+        N = 0
 
-        return result
+        for i in range(len(s)):
+            if s[i] == 'M':
+                N += 1000
+            elif s[i:i + 2] == 'CM':
+                N += 900
+            elif s[i] == 'D':
+                N += 500
+            elif s[i:i + 2] == 'CD':
+                N += 400
+            elif s[i] == 'C':
+                N += 100
+            elif s[i:i + 2] == 'XC':
+                N += 90
+            elif s[i] == 'L':
+                N += 50
+            elif s[i:i + 2] == 'XL':
+                N += 40
+            elif s[i] == 'X':
+                N += 10
+            elif s[i:i + 2] == 'IX':
+                N += 9
+            elif s[i] == 'V':
+                N += 5
+            elif s[i:i + 2] == 'IV':
+                N += 4
+            elif s[i] == 'I':
+                N += 1
+
+        return N
 
 
 romans = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
