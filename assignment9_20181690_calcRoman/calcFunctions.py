fc3d71a3b0a33f2bab20e3c7586c56d5417af9d0
yoosun813 <usun18813@kookmin.ac.kr>
2018-10-25 20:45:23 +0900

    assn9 폴더 따로 생성

---
+++ assignment9_20181690_calcRoman/calcFunctions.py
@@ -0,0 +1,59 @@
+from math import factorial as fact
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+
+def decToRoman(numStr):
+
+        n = int(numStr)
+        if n >= 4000:
+            return 'Error!'
+
+        result = ''
+
+
+        for value in sorted(romans.keys(), reverse=True):
+            while n >= value:
+                result += romans[value]
+                n -= value
+        return result
+
+
+
+def romanToDec(numStr):
+
+        n = int(numStr)
+        result = ""
+
+        return result
+
+
+
+
+
+romans = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+          100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
+          10: 'X', 9: 'IX', 5: 'V', 4: 'IV',
+          1: 'I'}
