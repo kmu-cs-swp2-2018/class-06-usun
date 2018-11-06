235d9f2873f2f0cf9192df84719a23176f48bd1f
yoosun813 <usun18813@kookmin.ac.kr>
2018-11-06 14:24:41 +0900

    Word클래스 __init__내용 추가

--- assignment10_20181690_hangman/word.py
+++ assignment10_20181690_hangman/word.py
@@ -1,7 +1,10 @@
 class Word:
     def __init__(self, filename):
-        # 파일을 읽어서 단어 데이터베이스 초기화한 다음, 파일 닫음        pass
-        pass
+        # 파일을 읽어서 단어 데이터베이스 초기화한 다음, 파일 닫음
+        self.word = []
+        file = open(filename, 'r')
+        lines = file.readline()
+        file.close()
 
     def randFromDB(self):
         # word.txt에서 임의의 단어를 선택
