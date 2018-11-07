a0e3f73719a2a9601473828babec4fa0321b8478
yoosun813 <usun18813@kookmin.ac.kr>
2018-11-07 23:16:59 +0900

    word.py 변수이름 수정

--- assignment10_20181690_hangman/word.py
+++ assignment10_20181690_hangman/word.py
@@ -3,7 +3,7 @@ import random
 class Word:
     def __init__(self, filename):
         # 파일을 읽어서 단어 데이터베이스 초기화한 다음, 파일 닫음
-        self.word = []
+        self.words = []
         file = open(filename, 'r')
         lines = file.readline()     # 행별로 구분
         file.close()
