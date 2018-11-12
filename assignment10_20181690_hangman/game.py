b45e475bc5a39f8375d3621d02cc08e8263594b2
yoosun813 <usun18813@kookmin.ac.kr>
2018-11-12 12:52:08 +0900

    assn10_hangman_game.py 음... 일단 커밋

--- assignment10_20181690_hangman/game.py
+++ assignment10_20181690_hangman/game.py
@@ -7,7 +7,7 @@ def gameMain():
     word = Word('words.txt')
     guess = Guess(word.randFromDB())        # 랜덤하게 단어 선택
 
-    finished = False
+    finished = True
     hangman = Hangman()
     maxTries = hangman.getLife()        # 최대 시도 횟수
 
@@ -21,20 +21,21 @@ def gameMain():
         if len(guessedChar) != 1:
             print('One character at a time!')
             continue
+# guess.haveGuess char()...?
         if guessedChar in guess.guessedChars:
             print('You already guessed \"' + guessedChar + '\"')
             continue
 
-        # 한글, 영어 대문자, 기호, 숫자를 입력했을 경우 / 아스키코드로 판별
+        # 한글, 영어 대문자, 기호, 숫자를 입력했을 경우 : 아스키코드로 판별
         if chr(97) > chr(ord(guessedChar)) or chr(122) < chr(ord(guessedChar)): # 아스키코드 99~122 가 영어 소문자
             print('Please input in English lower case')
             continue
 
-        finished = guess.guess(guessedChar)
-        if finished == True:
+# 플래그 변수 없엘려고 노력해야 됨
+        if finished == guess.guess(guessedChar):
             break
 
-    if finished == True:
+    if finished == guess.guess(guessedChar):
         print('Success')
     else:
         print(hangman.get(0))
